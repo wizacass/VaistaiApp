bd95c4677917600df1403ad71017bb098ea42fa9
Wizacass <visak.pet0@gmail.com>
2020-04-01 15:02:32 +0300

    Remove Welcome component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ console.log("Running JS...");
 
 window.Vue = require('vue');
 
-Vue.component('welcome', require('./components/Welcome.vue').default);
 Vue.component('mainmenu', require('./components/Menu.vue').default);
 
 const menuApp = new Vue({
