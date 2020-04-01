c9abd137edbe1ecfbbe44f57af913e8c88bbadca
Wizacass <visak.pet0@gmail.com>
2020-04-01 11:17:27 +0300

    Setup and import Bulma

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/main')
+
+@section('content')
+
+    <div class="container">
+    <h1 class="title is-centered">Hello World!</h1>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <title>VaistaiApp</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
