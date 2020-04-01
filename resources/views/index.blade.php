30de5035fd1afcd907182ab0b29c81b6a7f6f64c
Wizacass <visak.pet0@gmail.com>
2020-04-01 15:15:01 +0300

    Add Hero section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
 
-    <div class="container">
-    <h1 class="title is-centered">Hello World!</h1>
-    </div>
+    <section class="hero is-info is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">DB LD 2</h1>
+          <h2 class="subtitle">Vytenis Petrauskas IFF-8-1</h2>
+        </div>
+      </div>
+    </section>
 
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-    <title>VaistaiApp</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+  <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  <title>VaistaiApp</title>
 </head>
 <body>
+  <div id="vueMenu">
+    <mainmenu></mainmenu>
+  </div>
 
-    <div id="vueMenu">
-        <mainmenu></mainmenu>
-    </div>
+  @yield('content')
 
-    <div style="margin: 2em">
-        @yield('content')
-    </div>
-
-    <script type="text/javascript" src="js/app.js"></script>
+  <script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
