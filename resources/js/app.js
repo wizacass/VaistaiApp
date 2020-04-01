257b2cff511ab7467ccaf821dc7554ac3a80de46
Wizacass <visak.pet0@gmail.com>
2020-04-01 12:30:32 +0300

    Integrate VueJS framework

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+console.log("Running JS...");
+
+window.Vue = require('vue');
+
+Vue.component('welcome', require('./components/Welcome.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,32 @@
     <title>VaistaiApp</title>
 </head>
 <body>
-    @yield('content')
+
+    <nav class="navbar is-dark" role="navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">Home</a>
+            <a role="button" class="navbar-burger is-active" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div class="navbar-menu is-active">
+            <div class="navbar-start">
+                <a class="navbar-item" href="#">TBA</a>
+                <a class="navbar-item" href="#">TBA</a>
+            </div>
+        </div>
+    </nav>
+
+    <div style="margin: 2em">
+        @yield('content')
+    </div>
+
+    <div id="app">
+        <welcome></welcome>
+    </div>
+
+    <script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
