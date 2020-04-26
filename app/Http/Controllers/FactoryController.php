e95c034c95fdc70ac4427123b64c01d647142e9b
Wizacass <visak.pet0@gmail.com>
2020-04-26 18:52:03 +0300

    Implement delete logic for factory

--- app/Http/Controllers/FactoryController.php
+++ app/Http/Controllers/FactoryController.php
@@ -69,14 +69,10 @@ class FactoryController extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        DB::delete('DELETE FROM FABRIKAS WHERE pavadinimas = ?', [$id]);
+
+        return redirect('factories');
     }
 }
