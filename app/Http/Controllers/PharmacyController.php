c83fc946e6ca0b4aa5d99d2aaed8bcba27ffc72b
Wizacass <visak.pet0@gmail.com>
2020-04-28 21:23:09 +0300

    Enrich pharmacy creation form with employee information

--- app/Http/Controllers/PharmacyController.php
+++ app/Http/Controllers/PharmacyController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 class PharmacyController extends Controller
 {
+    private $nameValidators = ['required', 'min:3', 'max:255'];
+
     public function index()
     {
         $pharmacies = DB::select('SELECT * FROM VAISTINE');
@@ -17,15 +19,34 @@ class PharmacyController extends Controller
     public function create()
     {
         $networks = DB::select('SELECT TINKLAS.pavadinimas FROM TINKLAS');
+        $positions = DB::select('SELECT * FROM Vaistininko_Pareigos');
 
-        return view('pharmacy.create', compact('networks'));
+        return view('pharmacy.create', compact('networks', 'positions'));
     }
 
     public function store(Request $request)
     {
-        $attributes = request()->validate([
-            'address' => ['required', 'min:3', 'max:255'],
-        ]);
+        $employeeAdded =
+            $request->e_name != NULL ||
+            $request->e_surname != NULL ||
+            $request->e_exp != NULL;
+
+        if($employeeAdded)
+        {
+            $attributes = request()->validate([
+                'address' => $this->nameValidators,
+                'e_name' => $this->nameValidators,
+                'e_surname' => $this->nameValidators,
+                'e_exp' => ['required', 'integer', 'min:0', 'max:100'],
+                'e_position' => ['required', 'integer']
+            ]);
+        }
+        else
+        {
+            $attributes = request()->validate([
+                'address' => $this->nameValidators
+            ]);
+        }
 
         DB::insert('INSERT INTO VAISTINE (darbuotoju_skaicius, adresas, yraGamybine, telefonas, apyvarta, fk_TINKLASpavadinimas) VALUES (?, ?, ?, ?, ?, ?)', [
             0,
@@ -35,6 +56,21 @@ class PharmacyController extends Controller
             0,
             $request->network
         ]);
+
+        DB::update('UPDATE TINKLAS SET vaistiniu_skaicius = vaistiniu_skaicius + 1 WHERE pavadinimas = ?', [$request->network]);
+
+        if($employeeAdded)
+        {
+            $id = DB::select('SELECT LAST_INSERT_ID()')[0]->{'LAST_INSERT_ID()'};
+            DB::insert('INSERT INTO DARBUOTOJAS (vardas, pavarde, darbo_stazas, pareigos, fk_TINKLASpavadinimas, fk_VAISTINEfilialo_id) VALUES (?, ?, ?, ?, ?, ?)', [
+                $attributes['e_name'],
+                $attributes['e_surname'],
+                $attributes['e_exp'],
+                $attributes['e_position'],
+                $request->network,
+                $id
+            ]);
+        }
 
         return redirect('/pharmacies');
     }
@@ -60,7 +96,7 @@ class PharmacyController extends Controller
     public function update(Request $request, $id)
     {
         $attributes = request()->validate([
-            'address' => ['required', 'min:3', 'max:255'],
+            'address' => $this->nameValidators
         ]);
 
         DB::update('UPDATE VAISTINE SET adresas = ?, telefonas = ? WHERE filialo_id = ?', [
@@ -74,7 +110,19 @@ class PharmacyController extends Controller
 
     public function destroy($id)
     {
+        $network = DB::select('SELECT fk_TINKLASpavadinimas FROM VAISTINE WHERE filialo_id = ?', [$id])[0]->fk_TINKLASpavadinimas;
+
+        DB::update('UPDATE DARBUOTOJAS SET fk_TINKLASpavadinimas = ?, fk_VAISTINEfilialo_id = ? WHERE fk_VAISTINEfilialo_id = ?', [
+            NULL,
+            NULL,
+            $id
+        ]);
+
         DB::delete('DELETE FROM VAISTINE WHERE filialo_id = ?', [$id]);
+        if($network != NULL)
+        {
+            DB::update('UPDATE TINKLAS SET vaistiniu_skaicius = vaistiniu_skaicius - 1 WHERE pavadinimas = ?', [$network]);
+        }
 
         return redirect('/pharmacies');
     }
